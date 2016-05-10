8036338299cff82281560046fc961c5d5e350ea9
ChipyChipmunks <liamoconnor1247@gmail.com>
2016-05-11 00:05:54 +1200

    initial commit

---
+++ wellington-undiscovered.js
@@ -0,0 +1,11 @@
+
+
+function collapse() {
+	if (document.getElementById('#container').style.display != "block"){
+		document.getElementById('#container').style.display = 'block';
+	}
+	else {
+		document.getElementById('#container').style.display = 'none';
+	}
+	
+}
